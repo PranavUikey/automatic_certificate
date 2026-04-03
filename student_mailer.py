e6debfdf990117b94c8af5aa903b5f6bc63df162
pranav <pranavuiih@gmail.con>
2026-04-03 11:13:09 +0530

    added a workflow

--- student_mailer.py
+++ student_mailer.py
@@ -105,7 +105,9 @@ def send_email(to_email, name, s3_url):
 
     try:
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as smtp:
+            smtp.ehlo()
             smtp.starttls()
+            smtp.ehlo()
             smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
             smtp.send_message(msg)
 
